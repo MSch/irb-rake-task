fcaab487d74e788fc091e85b4392176e1e879e4b
Martin Schürrer <martin@schuerrer.org>
2010-12-10 05:07:40 +0100

    automatically include task if file is required

--- lib/irb/rake_task.rb
+++ lib/irb/rake_task.rb
@@ -3,27 +3,8 @@ require 'rake/tasklib'
 require 'irb'
 require 'irb/completion'
 
-module IRB
-  class RakeTask < ::Rake::TaskLib
-
-    # Name of task.
-    #
-    # default:
-    #   :spec
-    attr_accessor :name
-
-    def initialize(*args)
-      @name = args.shift || :console
-
-      yield self if block_given?
-
-      desc("Start the console") unless ::Rake.application.last_comment
-
-      task name do
-        ARGV.clear
-        IRB.start
-      end
-    end
-
-  end
+desc("Open IRB console")
+task name do
+  ARGV.clear
+  IRB.start
 end
